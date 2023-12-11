0fba5eb9fb6bdc18a52f6df7ee65ae310ee26729
Anuran Paul <anuran34@gmail.com>
2023-12-11 22:10:35 +0530

    Day 6

---
+++ day6/index.js
@@ -0,0 +1,67 @@
+// https://adventofcode.com/2023/day/6
+
+import { readFileSync } from "fs"
+
+const solution = solve(readLines(`./example.txt`))
+console.log(solution)
+
+function readLines(inputFilePath) {
+  const input = readFileSync(inputFilePath, "utf-8")
+  return input.split("\n")
+}
+
+function solve(inputLines) {
+  return {
+    part1: solvePart1(inputLines),
+    part2: solvePart2(inputLines)
+  }
+}
+
+function solvePart1(inputLines) {
+  const [timeLine, distanceLine] = inputLines
+  const times = parseLineForPart1(timeLine)
+  const distances = parseLineForPart1(distanceLine)
+  const races = times.map((time, i) => ({ time, distance: distances[i] }))
+  return solveForRaces(races)
+}
+
+function parseLineForPart1(line) {
+  return line
+    .split(":")[1]
+    .trim()
+    .split(/\s+/)
+    .map(x => parseInt(x, 10))
+}
+
+function solvePart2(inputLines) {
+  const [timeLine, distanceLine] = inputLines
+  const time = parseLineForPart2(timeLine)
+  const distance = parseLineForPart2(distanceLine)
+  return solveForRaces([{ time, distance }])
+}
+
+function parseLineForPart2(line) {
+  const num = line
+    .split(":")[1]
+    .trim()
+    .split(/\s+/)
+    .join("")
+  return parseInt(num, 10)
+}
+
+function solveForRaces(races) {
+  return races.map(getNumberOfWaysBeatingRecord).reduce((acc, x) => acc * x, 1)
+}
+
+function getNumberOfWaysBeatingRecord({
+  time: raceTime,
+  distance: recordDistance
+}) {
+  return [...Array(raceTime).keys()]
+    .map(holdTime => getDistance(holdTime, raceTime))
+    .filter(distanceByHold => distanceByHold > recordDistance).length
+}
+
+function getDistance(holdTime, raceTime) {
+  return holdTime * (raceTime - holdTime)
+}
