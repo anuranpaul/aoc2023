a370fb95cbdbdd3cab7a051acf0856e5f723855f
Anuran Paul <anuran34@gmail.com>
2023-12-14 21:21:00 +0530

    Day 11

---
+++ day11/part1.js
@@ -0,0 +1,19 @@
+import fs from "fs"
+
+const input = fs.readFileSync("./input.txt", "utf-8").split(/\r?\n/g);
+
+let adjustedMap = input;
+for (let i = 0; i < adjustedMap.length; i++) { if (adjustedMap[i].indexOf("#") == -1) adjustedMap.splice(i, 0, adjustedMap[i++]); }
+for (let i = 0; i < adjustedMap[0].length; i++) {
+    if (!adjustedMap.reduce((acc, cur) => acc || cur[i] == "#", false)) {
+        for (let j = 0; j < adjustedMap.length; j++) adjustedMap[j] = adjustedMap[j].slice(0, i) + "." + adjustedMap[j].slice(i);
+        i++;
+    }
+}
+
+let positions = [];
+for (let i = 0; i < adjustedMap.length; i++) { for (let j = 0; j < adjustedMap[i].length; j++) { if (adjustedMap[i][j] == "#") positions.push([i, j]); } }
+let pairs = [];
+for (let i = 0; i < positions.length; i++) { for (let j = 0; j < positions.length; j++) { if (i != j) pairs.push([ positions[i], positions[j] ]); } }
+
+console.log(pairs.map(pair => Math.abs(pair[0][0] - pair[1][0]) + Math.abs(pair[0][1] - pair[1][1])).reduce((acc, x) => acc + x) / 2);
---
+++ day11/part2.js
@@ -0,0 +1,46 @@
+import fs from "fs"
+
+const input = fs.readFileSync("input.txt", "utf8").split("\n");
+let adjustedMap = input;
+for (let i = 0; i < adjustedMap.length; i++) { if (adjustedMap[i].indexOf("#") == -1) adjustedMap.splice(++i, 0, new Array(adjustedMap[i].length).fill("$").join("")); }
+for (let i = 0; i < adjustedMap[0].length; i++) {
+    if (!adjustedMap.reduce((acc, cur) => acc || cur[i] == "#", false)) {
+        for (let j = 0; j < adjustedMap.length; j++) adjustedMap[j] = adjustedMap[j].slice(0, i + 1) + "$" + adjustedMap[j].slice(i + 1);
+        i++;
+    }
+}
+
+let positions = [];
+for (let i = 0; i < adjustedMap.length; i++) { for (let j = 0; j < adjustedMap[i].length; j++) { if (adjustedMap[i][j] == "#") positions.push([i, j]); } }
+let pairs = [];
+for (let i = 0; i < positions.length; i++) { for (let j = 0; j < positions.length; j++) { if (i != j) pairs.push([ positions[i], positions[j] ]); } }
+
+console.log(pairs.map(pair => {
+    let [ y1, x1 ] = pair[0], [ y2, x2 ] = pair[1];
+    let deltaY = 0, deltaX = 0, delta = 999999;
+
+    while (y1 < y2) {
+        if (adjustedMap[y1][x1] == "$") deltaY += delta;
+        else deltaY++;
+        y1++;
+    }
+    while (y1 > y2) {
+        if (adjustedMap[y1][x1] == "$") deltaY += delta;
+        else deltaY++;
+        y1--;
+    }
+
+    while (x1 < x2) {
+        if (adjustedMap[y1][x1] == "$") deltaX += delta;
+        else deltaX++;
+        x1++;
+    }
+    while (x1 > x2) {
+        if (adjustedMap[y1][x1] == "$") deltaX += delta;
+        else deltaX++;
+        x1--;
+    }
+
+    return deltaX + deltaY;
+}).reduce((acc, x) => acc + x) / 2);
+
